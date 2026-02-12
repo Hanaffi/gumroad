27f30bcdb05779752257600c4f55bebc2ef85a71
Hanaffi <ahmedellord56@gmail.com>
2026-02-12 03:03:00 +0200

    Update membership component

--- app/javascript/components/Admin/Users/Memberships.tsx
+++ app/javascript/components/Admin/Users/Memberships.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import DateTimeWithRelativeTooltip from "$app/components/Admin/DateTimeWithRelativeTooltip";
 import type { User, UserMembership } from "$app/components/Admin/Users/User";
 import { Card, CardContent } from "$app/components/ui/Card";
+import { Pill } from "$app/components/ui/Pill";
 
 type MembershipsProps = {
   user: User;
@@ -16,28 +17,32 @@ type MembershipProps = {
 
 const Membership = ({ membership, className }: MembershipProps) => (
   <div className={className}>
+    <Pill size="small" className="w-20 justify-center">
+      {membership.role}
+    </Pill>
     <div className="flex grow items-center gap-4">
-      <img src={membership.seller.avatar_url} className="user-avatar" alt={membership.seller.display_name_or_email} />
-      <div className="grid">
-        <h5>
-          <Link href={Routes.admin_user_url(membership.seller.external_id)}>
-            {membership.seller.display_name_or_email}
-          </Link>
-        </h5>
-        <div>{membership.role}</div>
-      </div>
+      <img
+        src={membership.seller.avatar_url}
+        className="user-avatar h-8! w-8!"
+        alt={membership.seller.display_name_or_email}
+      />
+      <h5>
+        <Link href={Routes.admin_user_url(membership.seller.external_id)}>
+          {membership.seller.display_name_or_email}
+        </Link>
+      </h5>
     </div>
-    <div className="text-right">
+    <div className="flex gap-1">
+      <div className="space-x-1">
+        <span>invited</span>
+        <DateTimeWithRelativeTooltip date={membership.created_at} />
+      </div>
       {membership.last_accessed_at ? (
         <div className="space-x-1">
-          <span>last accessed</span>
+          <span>· last accessed</span>
           <DateTimeWithRelativeTooltip date={membership.last_accessed_at} />
         </div>
       ) : null}
-    </div>
-    <div className="space-x-1">
-      <span>invited</span>
-      <DateTimeWithRelativeTooltip date={membership.created_at} />
     </div>
   </div>
 );
@@ -48,7 +53,7 @@ const Memberships = ({ user: { admin_manageable_user_memberships } }: Membership
       <hr />
       <details>
         <summary>
-          <h3>User memberships</h3>
+          <h3>Team memberships</h3>
         </summary>
         <Card>
           {admin_manageable_user_memberships.map((membership) => (
